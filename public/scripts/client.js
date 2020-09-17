1ee5402277a86207e4bf806f3b7326874d454481
David Ogbeide <boyowao@gmail.com>
2020-09-17 18:21:51 +0000

    render tweets from data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,18 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
-
 $(function() {
   $('#tweet-submit').on('click', function() {
     const form = $("#tweet-form");
@@ -39,12 +27,64 @@ $(function() {
     });
   });
 
-  const $tweet = createTweetElement(tweetData);
-  // console.log($tweet);
-  $('#tweets').append($tweet);
+  renderTweets(data);
 });
 
-const createTweetElement = function(tweet) {
-  const $tweet = $(`<article class="tweet">Hello world</article>`);
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+const createTweetElement = function(datum) {
+  // TODO: time ago calculation
+  const $tweet = $(
+    `<article class="tweet">
+      <header>
+        <div>
+          <img src="${datum.user.avatars}" alt="[icon]">
+          <span>${datum.user.name}</span>
+        </div>
+        <span>${datum.user.handle}</span>
+      </header>
+      <main>
+        ${datum.content.text}
+      </main>
+      <footer>
+        <span>3 days ago</span>
+        <span>
+          <span>f</span>
+          <span>r</span>
+          <span>h</span>
+        </span>
+      </footer>
+    </article>`
+  );
   return $tweet;
+}
+
+const renderTweets = function(tweets) {
+  const $container = $('#tweets');
+  for (tweet of tweets) {
+    const $elem = createTweetElement(tweet);
+    $container.prepend($elem);
+  }
 }
