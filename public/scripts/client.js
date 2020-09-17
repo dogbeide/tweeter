d09d7e43aee4f2345b1f883330d4ddd0cdea7fcc
David Ogbeide <boyowao@gmail.com>
2020-09-17 16:42:51 +0000

    implement create tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,26 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(function() {
+  $('#tweet-submit').on('click', function() {
+    const form = $("#tweet-form");
+
+    form.submit(function(event){
+      event.preventDefault();
+      const url = '/tweets';
+      const method = 'POST';
+      const data = form.serialize();
+
+      $.ajax({
+        url,
+        type: method,
+        data
+      }).done(function(response) {
+        console.log("response", response);
+        form.trigger('reset');
+      }).fail(function(error, status) {
+        console.log(error, status);
+      });
+    });
+  });
+});
