29289517c63c8859ed06ff88b965b29edbe34c1b
David Ogbeide <boyowao@gmail.com>
2020-09-17 17:58:20 +0000

    implement createTWeetElement stub

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,18 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
 $(function() {
   $('#tweet-submit').on('click', function() {
     const form = $("#tweet-form");
@@ -26,4 +38,13 @@ $(function() {
       });
     });
   });
+
+  const $tweet = createTweetElement(tweetData);
+  // console.log($tweet);
+  $('#tweets').append($tweet);
 });
+
+const createTweetElement = function(tweet) {
+  const $tweet = $(`<article class="tweet">Hello world</article>`);
+  return $tweet;
+}
