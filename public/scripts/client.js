d46597ecbbaa38bc2af3349cc70a27c2fa47d9af
David Ogbeide <boyowao@gmail.com>
2020-09-17 18:38:34 +0000

    load tweets from server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -27,32 +27,15 @@ $(function() {
     });
   });
 
-  renderTweets(data);
+  loadTweets();
 });
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+const loadTweets = function () {
+  $.ajax('/tweets', {method: 'GET'})
+  .then(function(data) {
+    renderTweets(data);
+  });
+}
 
 const createTweetElement = function(datum) {
   // TODO: time ago calculation
