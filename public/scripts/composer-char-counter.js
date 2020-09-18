1ea6b81d34ab6610e2b80a0c84e6a976701ea813
David Ogbeide <boyowao@gmail.com>
2020-09-18 18:38:49 +0000

    tweet character count goes red beyond max length

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,13 @@ $(document).ready(function() {
   $('#tweet-text').on('keydown keyup', function(event) {
     const count = $('#tweet-text').val().length;
     const remaining = 140 - count;
-    $(this).closest('form').find('.counter').val(remaining);
+    const counter = $(this).closest('form').find('.counter');
+    counter.val(remaining);
+
+    if (remaining < 0) {
+      counter.addClass('counter-red');
+    } else {
+      counter.removeClass('counter-red')
+    }
   })
 });
